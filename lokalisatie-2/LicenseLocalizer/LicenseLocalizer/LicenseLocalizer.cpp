fdc34868be3c74d06a1ddd5b683bba2804c9928f
Mike Schaap <mikeschaap@gmail.com>
2014-03-21 11:43:57 +0100

    New project for localisation

---
+++ lokalisatie-2/LicenseLocalizer/LicenseLocalizer/LicenseLocalizer.cpp
@@ -0,0 +1,29 @@
+// LicenseLocalizer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string inputFileLocation;
+	string outputFileLocation;
+
+	cout << "Please specify the path to the input file: \n";
+
+	cin >> inputFileLocation;
+	
+	cout << "Please specify the path for the output file: \n";
+
+	cin >> outputFileLocation;
+
+
+
+
+	return 0;
+}
+
