b6018316264a86007c4e2ca4da94730112e24609
bryan baan <bryan.baan@student.hu.nl>
2014-04-04 11:29:48 +0200

    LicenseLokalizer klasse toegevoegd + verwijderd

--- Vision week 3&4/Vision_timer/BlobCheck.h
+++ Vision week 3&4/Vision_timer/BlobCheck.h
@@ -3,6 +3,7 @@
 #include "Image.h"
 #include "exectimer.h"
 #include <iostream>
+#include <list>
 
 class BlobCheck {
 private:
@@ -10,7 +11,7 @@ private:
 public:
 	BlobCheck();
 	~BlobCheck();
-	void CheckIfBlobIsLicensePlate(Blob &blobobjects);
+	void CheckIfBlobIsLicensePlate(std::list<blob> &blobobjects);
 };
 
 #endif
